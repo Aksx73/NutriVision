8d035e910ca5371523f81afc77e75ef6f84c6ef7
Akshay <akshaynighot73@gmail.com>
2025-11-26 22:33:56 +0530

    UI enhancements

--- app/src/main/java/com/absut/nutrivision/ui/component/EmptyState.kt
+++ app/src/main/java/com/absut/nutrivision/ui/component/EmptyState.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.absut.nutrivision.ui.theme.NutriVisionTheme
 
 @Composable
 fun EmptyState(modifier: Modifier = Modifier) {
@@ -31,4 +33,13 @@ fun EmptyState(modifier: Modifier = Modifier) {
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    NutriVisionTheme {
+        EmptyState()
+    }
+    
 }
